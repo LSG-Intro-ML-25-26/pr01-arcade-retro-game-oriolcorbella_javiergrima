d410380adba5984c149ce34e017532878db1afa4
Javier Grima Vivas <javier.grima@gracia.lasalle.cat>
2026-01-08 19:00:19 +0100

    primer y segundo mapa

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+namespace SpriteKind {
+    export const objeto = SpriteKind.create()
+    export const npc = SpriteKind.create()
+}
+function DialogoPrincesa () {
+    game.showLongText("Bienvenido viajero, ", DialogLayout.Bottom)
+}
+function PantallaPrincipal () {
+    scene.setBackgroundImage(assets.image`castillo`)
+    puerta = sprites.create(assets.image`puerta`, SpriteKind.objeto)
+    puerta.setPosition(46, 65)
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
-    assets.animation`nena-animation-up`,
-    500,
-    false
-    )
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-left`,
+    assets.animation`nena-animation-down`,
     500,
     false
     )
@@ -22,14 +26,37 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
-    assets.animation`nena-animation-down`,
+    assets.animation`nena-animation-left`,
     500,
     false
     )
 })
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-up`,
+    500,
+    false
+    )
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.npc, function () {
+    DialogoPrincesa()
+})
+function Pantalla2 () {
+    tiles.setCurrentTilemap(tilemap`nivel6`)
+    sprites.destroy(puerta)
+    princesa = sprites.create(assets.image`princesa`, SpriteKind.npc)
+    princesa.setPosition(139, 60)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.objeto, function () {
+    Pantalla2()
+})
+let princesa: Sprite = null
+let puerta: Sprite = null
 let nena: Sprite = null
+PantallaPrincipal()
 nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
 controller.moveSprite(nena)
